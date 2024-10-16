e69c18b3609f1191818c3e4b42e59810ac6a4be9
logancharrier <153896377+logancharrier@users.noreply.github.com>
2024-10-16 14:45:12 +0200

    Début de la page v_validationFiche
    Ajout du tableau en bas de la page validation pour l'instant vide

--- src/Controleurs/c_validerFrais.php
+++ src/Controleurs/c_validerFrais.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Gestion de l'affichage des frais
+ * Gestion de l'accueil
  *
  * PHP Version 8
  *
@@ -15,33 +15,9 @@
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
 
-use Outils\Utilitaires;
-
-$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$idVisiteur = $_SESSION['id'];
-switch ($action) {
-    case 'selectionnerInfos':
-        $lesVisiteurs = $pdo->getLesVisiteurs($idVisiteur);
-        // Afin de sélectionner par défaut le dernier mois dans la zone de liste
-        // on demande toutes les clés, et on prend la première,
-        // les mois étant triés décroissants
-        $lesCles = array_keys($lesMois);
-        $visiteurASelectionner = $lesCles[0];
-        include PATH_VIEWS . 'v_listeMois.php';
-        break;
-    case 'voirEtatFrais':
-        $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
-        $moisASelectionner = $leMois;
-        include PATH_VIEWS . 'v_listeMois.php';
-        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $leMois);
-        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $leMois);
-        $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $leMois);
-        $numAnnee = substr($leMois, 0, 4);
-        $numMois = substr($leMois, 4, 2);
-        $libEtat = $lesInfosFicheFrais['libEtat'];
-        $montantValide = $lesInfosFicheFrais['montantValide'];
-        $nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
-        $dateModif = Utilitaires::dateAnglaisVersFrancais($lesInfosFicheFrais['dateModif']);
-        include PATH_VIEWS . 'v_etatFrais.php';
+if ($estConnecte) {
+    include_once PATH_VIEWS . 'v_entete.php';
+	include PATH_VIEWS . 'v_validationFiche.php';
+} else {
+    include PATH_VIEWS . 'v_connexion.php';
 }
